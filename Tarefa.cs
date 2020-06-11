d87da80c5b4e0e2afb06ba7241350c9a53e22a74
bnsdesenvolvimento2 <55952940+bnsdesenvolvimento2@users.noreply.github.com>
2020-06-11 09:42:36 -0300

    Add files via upload

---
+++ Tarefa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ATM.Models
+{
+    public class Tarefa
+    {
+        public int TarefaID { get; set; }
+        public string NomeTarefa { get; set; }
+        public string Descricao { get; set; }
+        public string Disciplina { get; set; }
+        public DateTime Data { get; set; }
+    }
+
+    
+}
